50f3b307d5b3b8a66d98afecfea9aec68e4f165b
Biontos <116175438+Biontos@users.noreply.github.com>
2023-09-26 20:39:18 +0300

    Add files via upload

---
+++ Задание 4.cs
@@ -0,0 +1,37 @@
+﻿//Задание 4
+int chislo = Convert.ToInt32(Console.ReadLine());
+if (chislo < -1 && chislo > 10) Console.WriteLine("число не подходит");
+else 
+switch (chislo)
+{
+    case 0:
+        Console.WriteLine("zero");
+        break;
+    case 1:
+        Console.WriteLine("one");
+        break;
+    case 2:
+        Console.WriteLine("two");
+        break;
+    case 3:
+        Console.WriteLine("three");
+        break;
+    case 4:
+        Console.WriteLine("four");
+        break;
+    case 5:
+        Console.WriteLine("five");
+        break;
+    case 6:
+        Console.WriteLine("six");
+        break;
+    case 7:
+        Console.WriteLine("seven");
+        break;
+    case 8:
+        Console.WriteLine("eight");
+        break;
+    case 9:
+        Console.WriteLine("nine");
+        break;
+}
