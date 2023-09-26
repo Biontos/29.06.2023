4dc401b266fe60035ddec8e02cb3e60da596052d
Biontos <116175438+Biontos@users.noreply.github.com>
2023-09-26 20:49:57 +0300

    Add files via upload

---
+++ Задание 6.cs
@@ -0,0 +1,21 @@
+﻿// Задание 6
+using System;
+int ocenka = Convert.ToInt32(Console.ReadLine());
+switch (ocenka)
+{
+    case 1:
+        Console.WriteLine("плохо");
+        break;
+    case 2:
+        Console.WriteLine("неудовлетворительно");
+        break;
+    case 3:
+        Console.WriteLine("удовлетворительно");
+        break;
+    case 4:
+        Console.WriteLine("хорошо");
+        break;
+    case 5:
+        Console.WriteLine("отлично");
+        break;
+}
